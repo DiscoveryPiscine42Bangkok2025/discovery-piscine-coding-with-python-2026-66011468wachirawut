d826f74e1ac97b2f648bb3d0a88d51eaab5b6e8d
66011468wachirawut <66011468@kmitl.ac.th>
2026-02-10 20:54:40 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number less than 25 : "))
+if x > 25 :
+    print("Error")
+else:
+    while x <= 25 :
+        print("Inside the loop, my variable is"+" "+ str(x))
+        x = x+1
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,5 @@
+x = int(input("Enter a number: "))
+y = 0
+while y <= 9:
+    print(f"{y} x {x} = {x*y}")
+    y = y+1
